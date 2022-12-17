ef5fb9eed7fec202a0bac84605ae5f7a51599de4
Kidsnd274 <samuel_tee_gy@hotmail.com>
2022-12-17 23:15:27 +0800

    Whoops small mistake

--- Av1anStaxRipWrapper.py
+++ Av1anStaxRipWrapper.py
@@ -30,7 +30,7 @@ av1an_exec = "av1an.exe"
 
 command = av1an_exec
 command = add_argument(command, "--verbose -y --resume -a=\"-an\" --pix-format yuv420p10le")
-if parser_args.args is not None:
+if parser_args.arguments is not None:
     command = add_argument(command, parser_args.args)
 command = add_argument(command, f"-i \"{input_file}\" -o \"{output_file}\" --temp \"{tempdir}\"")
 
