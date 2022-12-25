403f29773ec30124454d6a47457ce2dbe2d46580
Kidsnd274 <samuel_tee_gy@hotmail.com>
2022-12-25 13:10:15 +0800

    Add message in batch file

--- setup_py_vp_environment.bat
+++ setup_py_vp_environment.bat
@@ -7,5 +7,6 @@ curl --output get-pip.py https://bootstrap.pypa.io/get-pip.py
 .\Scripts\pip.exe install psutil
 powershell -Command "(Get-Content python39._pth) -replace '#import site', 'import site' | Out-File -encoding ASCII python39._pth"
 REM Copying lsmas ffms2 to VapourSynth plugin directory (Make sure to disable loading VP plugins!)
+echo Copying plugins...
 cp ..\..\Plugins\Dual\FFMS2\ffms2.dll .\vapoursynth64\plugins
 cp ..\..\Plugins\VS\LSmashSource\vslsmashsource.dll .\vapoursynth64\plugins
