c03fd1eadce35f894842796c075ad4b1c6e0c34a
Kidsnd274 <samuel_tee_gy@hotmail.com>
2022-12-25 13:12:15 +0800

    fix wrong command

--- setup_py_vp_environment.bat
+++ setup_py_vp_environment.bat
@@ -7,5 +7,5 @@ curl --output get-pip.py https://bootstrap.pypa.io/get-pip.py
 .\Scripts\pip.exe install psutil
 powershell -Command "(Get-Content python39._pth) -replace '#import site', 'import site' | Out-File -encoding ASCII python39._pth"
 echo Copying lsmas ffms2 to VapourSynth plugin directory (Make sure to disable loading VP plugins!)
-cp ..\..\Plugins\Dual\FFMS2\ffms2.dll .\vapoursynth64\plugins
-cp ..\..\Plugins\VS\LSmashSource\vslsmashsource.dll .\vapoursynth64\plugins
+copy ..\..\Plugins\Dual\FFMS2\ffms2.dll .\vapoursynth64\plugins
+copy ..\..\Plugins\VS\LSmashSource\vslsmashsource.dll .\vapoursynth64\plugins
