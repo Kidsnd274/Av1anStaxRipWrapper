14c0870fcfe49c520d92322c6b0592b633b0b07c
Kidsnd274 <samuel_tee_gy@hotmail.com>
2023-01-12 16:15:39 +0800

    add aomenc to path for cmd script

--- run_cmd_with_path.bat
+++ run_cmd_with_path.bat
@@ -2,7 +2,7 @@
 REM This batch script needs to be run in StaxRip\Apps\Encoders\Av1anStaxRipWrapper!
 echo This batch script needs to be run in StaxRip\Apps\Encoders\Av1anStaxRipWrapper!
 echo.
-SET PATH=..\Av1an;..\rav1e;.\VapourSynth;%PATH%
+SET PATH=..\Av1an;..\aomenc;..\rav1e;.\VapourSynth;%PATH%
 echo Path set, you can now access encoder tools
 echo.
 cmd
